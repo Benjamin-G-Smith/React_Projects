73e92ef5880a21644f9a47d03f7e60463954e090
smithb18 <39067570+smithb18@users.noreply.github.com>
2020-08-16 13:38:43 -0700

    reactsetup

---
+++ darkweather/src/components/TopNav.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+class TopNav extends Component {
+    render(){
+        return(
+            <React.Fragment>
+                <nav className="navbar">
+                    <a>
+                        <h1>Home</h1>
+                    </a>
+                </nav>
+            </React.Fragment>
+        );
+    }
+
+}
