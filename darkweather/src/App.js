d5ad769adbecb6ea3c74724eee14d638cdedeb0a
smithb18 <39067570+smithb18@users.noreply.github.com>
2020-08-16 22:01:48 -0700

    added chart demo

--- darkweather/src/App.js
+++ darkweather/src/App.js
@@ -5,7 +5,6 @@ import Homepage from './components/Homepage';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-//const Message = props => <div><h1>{props.msg}</h1></div>;
 const Spacer = {
   height: '10px',
   margin: '10px'
--- darkweather/src/components/Homepage.js
+++ darkweather/src/components/Homepage.js
@@ -1,11 +1,28 @@
 import React, {Component} from "react";
 import './Homepage.css';
+import LineChartDemo from './LineChartDemo';
+
+const center = {
+    margin: 'auto',
+    padding: '10px'
+};
 
 class Homepage extends Component {
     render(){
         return (
             <div className="Home-Page bg-dark text-light">
-                    <h1 >hello</h1>                
+                    <h1 >Demo of Chart js</h1>       
+                    <div className="container">
+                        <div className="row">
+                        <div style={center}>
+                            <LineChartDemo  />   
+                            </div>
+                        
+
+                        </div>
+
+                    </div>
+                          
             </div>
         );
     }
